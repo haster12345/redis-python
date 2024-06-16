87c55a47aafd7b809c88f8efe51e84686a0f82ef
haster12345 <tarunmankani25@gmail.com>
2024-06-15 19:42:52 -0500

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,18 +1,13 @@
-# Uncomment this to pass the first stage
 import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
-    client, socket = server_socket.accept()
+    client, addr = server_socket.accept()
     client.send(b"+PONG\r\n")
-
 
 
 if __name__ == "__main__":

c633475e96318868283eae3bda3c4b587569f694
haster12345 <tarunmankani25@gmail.com>
2024-06-16 13:36:42 -0500

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 
 def main():
@@ -17,6 +18,7 @@ def main():
         if data == b'*1\r\n$4\r\nPING\r\n':
             client.send(b"+PONG\r\n")
 
+        time.sleep(3)
         # client.close()
 
     # server_socket.close()

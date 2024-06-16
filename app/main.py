3e429bdf2463e01dbb1ed611db6b9bc7853c6e25
haster12345 <tarunmankani25@gmail.com>
2024-06-16 12:54:24 -0500

    pass stage

--- app/main.py
+++ app/main.py
@@ -4,19 +4,14 @@ import socket
 def main():
     print("Logs from your program will appear here!")
 
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # sock.bind(("localhost", 6379))
     server_socket.listen(1)
 
-    print("check1")
-    client, addr = server_socket.accept()
-    print("check2")
-    data = client.recv(1024)
-    print(data)
-    if data == b'*1\r\n$4\r\nPING\r\n':
-        print("data recieved")
-        client.send(b"+PONG\r\n")
+    while True:
+        client, addr = server_socket.accept()
+        data = client.recv(1024)
+        if data == b'*1\r\n$4\r\nPING\r\n':
+            client.send(b"+PONG\r\n")
 
     client.close()
     server_socket.close()

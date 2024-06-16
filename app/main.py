92c8fd75982c877d521be1c95a94eecb5fc1de79
haster12345 <tarunmankani25@gmail.com>
2024-06-15 19:19:13 -0500

    pass stage

--- app/main.py
+++ app/main.py
@@ -10,6 +10,9 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
+    client, socket = server_socket.accept()
+    client.send(b"+PONG\r\n")
+
 
 
 if __name__ == "__main__":

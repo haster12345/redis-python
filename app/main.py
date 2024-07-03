59deef3d2ffc2b32e76f28fd12f3ffbae14f96f8
haster12345 <tarunmankani25@gmail.com>
2024-07-03 16:35:36 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,27 +1,25 @@
 import socket
 import time
+import threading
 
 
 def main():
     print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.listen(5)
     print("server listening")
 
     while True:
-        print("check 1")
         client, addr = server_socket.accept()
-        print("check 2")
-        while True:
-            data = client.recv(1024)
-            print(data)
-            if data == b'*1\r\n$4\r\nPING\r\n':
-                client.send(b"+PONG\r\n")
+        print(f"Accepted connection from {addr}")
+        client_handler = threading.Thread(target=handle_client, args=(client, ))
+        client_handler.start()
 
-        # client.close()
-
-    # server_socket.close()
+def handle_client(client_socket):
+    while True:
+        data = client_socket.recv(1024)
+        if data == b'*1\r\n$4\r\nPING\r\n':
+            client_socket.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":

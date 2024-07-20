538f47bbb254477aa2c57643f0240035561c925b
haster12345 <tarunmankani25@gmail.com>
2024-07-20 23:22:39 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -17,18 +17,43 @@ def main():
         client_handler.start()
 
 def handle_client(client_socket):
+    store = {}
     while True:
         data = client_socket.recv(1024)
         data_parse = parse_resp(data)
 
-        assert data_parse
+        print(data_parse)
+
+        if not data_parse:
+            return
 
         if data_parse[0] == 'PING':
             client_socket.send(b"+PONG\r\n")
 
         elif data_parse[0] == 'ECHO':
             print("$%s\r\n%s\r\n" % (len(data_parse[1]), data_parse[1]))
-            client_socket.send(b"$%s\r\n%s\r\n" % (str.encode(f"{len(data_parse[1])}"), str.encode(data_parse[1])))
+            client_socket.send(
+                b"$%s\r\n%s\r\n" %
+                (str.encode(f"{len(data_parse[1])}"), str.encode(data_parse[1]))
+            )
+
+        elif data_parse[0] == 'SET':
+            store[data_parse[1]] = data_parse[2]
+            print(store)
+            client_socket.send(b"+OK\r\n")
+
+        elif data_parse[0] == 'GET':
+            value = get_value(key=data_parse[1], store=store)
+
+            if value == "$-1\r\n":
+                client_socket.send(b"$-1\r\n")
+
+            else:
+                client_socket.send(
+                        b"$%s\r\n%s\r\n" %
+                        (str.encode(f"{len(value)}"), str.encode(value))
+                    )
+
         else:
             assert "Not Implemented"
 
@@ -64,6 +89,13 @@ def parse_bulk_str(data):
     $<length>\r\n<data>\r\n
     """
     pass
+
+
+def get_value(key, store):
+    try:
+        return str(store[key])
+    except KeyError:
+        return "$-1\r\n"
 
 
 if __name__ == "__main__":
